1364e66e899bc2184cd07471f91c16bff8cd988a
Dmytro Artamonov <revels0k@gmail.com>
2021-03-27 14:07:17 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const fs = require('fs');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const { BannerPlugin } = require('webpack');
+
+module.exports = {
+  entry: './src/index.tsx',
+  target: 'web',
+  mode: 'development',
+  devtool: false,
+  output: {
+    path: path.resolve('./dist'),
+    filename: 'bundle.js',
+  },
+  externals: {
+    'react': 'React',
+    'react-dom': 'ReactDOM',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          babelrc: false,
+          presets: [
+            ['@babel/preset-typescript'],
+            ['@babel/preset-react'],
+          ],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      typescript: { configFile: 'tsconfig.json' },
+    }),
+    new BannerPlugin({
+      banner: () => fs.readFileSync(path.join(__dirname, 'banner.txt'), 'utf8'),
+      raw: true,
+    }),
+  ],
+};
